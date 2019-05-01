9526d2c6657ab5f303ea5b5ab80064bda2beb411
proactive-point-1823 <daylanberry@gmail.com>
2019-05-01 04:01:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -52,8 +52,10 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   if (cardNumber.length === 0){
-    return "Sorry, we don't have a credit card on file for you."
-  } 
-  return  `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
-
+    string = "Sorry, we don't have a credit card on file for you."
+  } else {
+    string = `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+    cart = []
+  }
+  return string;
 }
